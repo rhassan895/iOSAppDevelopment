d75d4ebd0c2318d1427bb3c3a359d518cdcc824f
Rashne Hassan <77666667+rhassan895@users.noreply.github.com>
2021-01-25 19:12:32 -0500

    startingProject1

---
+++ Week1/Project1/Project1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project1
+//
+//  Created by Rashne Hassan on 25/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Week1/Project1/Project1/Project1App.swift
@@ -0,0 +1,17 @@
+//
+//  Project1App.swift
+//  Project1
+//
+//  Created by Rashne Hassan on 25/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct Project1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
